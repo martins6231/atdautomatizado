6d5788b2a6e5370ca0231fc9f27e0831d10a17ed
Matheus Martins Lopes <martins6231@gmail.com>
2025-05-13 20:47:26 -0300

    Update atd.py

--- atd.py
+++ atd.py
@@ -1018,7 +1018,7 @@ with st.container():
     st.markdown('<div class="logo-container">', unsafe_allow_html=True)
     # Placeholder para a logo - substitua pela URL da logo da empresa
     logo_url = "https://raw.githubusercontent.com/martins6231/app_atd/main/britvic_logo.png"  # URL padrão, substitua pela logo da empresa
-    st.image(logo_url, width=150, output_format="PNG", use_container_width=False)
+    st.image(logo_url, width=200, output_format="PNG", use_container_width=False)
     st.markdown('</div>', unsafe_allow_html=True)
     
     # Título principal
